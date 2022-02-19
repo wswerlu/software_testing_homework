7a973956407f13f1073cce87ba61d66d392756cf
wswerlu <popovpn7@gmail.com>
2022-02-19 18:51:46 +0300

    add contact.py

---
+++ contact.py
@@ -0,0 +1,7 @@
+class Contact:
+    def __init__(self, firstname, lastname, address, home_phone, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.home_phone = home_phone
+        self.email = email

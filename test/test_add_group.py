2b8cac70105cb452e48f543e5bb901942ccc51d0
wswerlu <popovpn7@gmail.com>
2022-02-27 00:12:47 +0300

    refactor test_add_group.py

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,19 +1,10 @@
 # -*- coding: utf-8 -*-
-import pytest
-from fixture.application import Application
 from model.group import Group
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
-    
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="asdfdsv", header="xczvzxcv", footer="xzcvzxv"))
+    app.group.create(Group(name="test create", header="test create", footer="test create"))
     app.session.logout()
 
 

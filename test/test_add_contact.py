9ee43087ea6dd9d7ffe70215d76f099eebe39e9f
wswerlu <popovpn7@gmail.com>
2022-02-27 00:25:07 +0300

    refactor test_add_contact.py: remove fixture

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,18 +1,9 @@
 # -*- coding: utf-8 -*-
-import pytest
-from fixture.application import Application
 from model.contact import Contact
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.create(Contact(firstname="sdfasdfs", lastname="asdfsdfas", address="jgkghjk",
-                               home_phone="ewefsdf", email="asdfdsfs"))
+    app.contact.create(Contact(firstname="test create", lastname="test create", address="test create",
+                               home_phone="test create", email="test create"))
     app.session.logout()

9d0e0112433e67032cdcda2f3f9d8dd56bf5f299
wswerlu <popovpn7@gmail.com>
2022-02-26 21:59:33 +0300

    create packages for different zones responsibility: fixture, model, test

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from application import Application
-from group import Group
+from fixture.application import Application
+from model.group import Group
 
 
 @pytest.fixture

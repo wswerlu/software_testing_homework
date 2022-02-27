80a0c0861c6b52beb5dee0a54e4daa6d8ba0713d
wswerlu <popovpn7@gmail.com>
2022-02-27 16:49:31 +0300

    refactor contact.py: add methods delete_first_contact and edit_first_contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -29,3 +29,35 @@ class ContactHelper:
     def return_to_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # select first contact
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_css_selector("[value='Delete']").click()
+        wd.switch_to.alert.accept()
+
+    def edit_first_contact(self, contact):
+        wd = self.app.wd
+        # init first contact editing
+        wd.find_element_by_css_selector("[alt='Edit']").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_phone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        # submit contact editing
+        wd.find_element_by_name("update").click()
+        self.return_to_home_page()

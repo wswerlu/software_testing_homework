4f32c3e736bb2d6bba41ab3c339ff43e04467228
wswerlu <popovpn7@gmail.com>
2022-03-03 00:18:45 +0300

    refactor fixture/contact.py: add method fill_contact_form

--- fixture/contact.py
+++ fixture/contact.py
@@ -6,11 +6,8 @@ class ContactHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("home").click()
 
-    def create(self, contact):
+    def fill_contact_form(self, contact):
         wd = self.app.wd
-        # init add creation
-        wd.find_element_by_link_text("add new").click()
-        # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys(contact.firstname)
@@ -26,6 +23,12 @@ class ContactHelper:
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys(contact.email)
+
+    def create(self, contact):
+        wd = self.app.wd
+        # init add creation
+        wd.find_element_by_link_text("add new").click()
+        self.fill_contact_form(contact)
         # submit contact creation
         wd.find_element_by_xpath("//input[21]").click()
         self.return_to_home_page()
@@ -48,22 +51,7 @@ class ContactHelper:
         self.open_contact_page()
         # init first contact editing
         wd.find_element_by_css_selector("[alt='Edit']").click()
-        # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home_phone)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
+        self.fill_contact_form(contact)
         # submit contact editing
         wd.find_element_by_name("update").click()
         self.return_to_home_page()

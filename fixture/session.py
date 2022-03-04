f1c08f3dab6a8772b30615c1993e678227f274a2
wswerlu <popovpn7@gmail.com>
2022-03-04 23:36:17 +0300

    refactor session.py: add methods ensure_logout, is_logged_in, is_logged_in_as, ensure_login

--- fixture/session.py
+++ fixture/session.py
@@ -17,3 +17,25 @@ class SessionHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
         wd.find_element_by_name("user")
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_css_selector("form b").text == f"({username})"
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)

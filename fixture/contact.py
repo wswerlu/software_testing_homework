7095ed1b253d35b8fe6049f46b719aeb954fef81
wswerlu <popovpn7@gmail.com>
2022-03-04 22:39:01 +0300

    refactor fixture\contact.py: add method change_field_name

--- fixture/contact.py
+++ fixture/contact.py
@@ -7,22 +7,18 @@ class ContactHelper:
         wd.find_element_by_link_text("home").click()
 
     def fill_contact_form(self, contact):
+        self.change_field_name("firstname", contact.firstname)
+        self.change_field_name("lastname", contact.lastname)
+        self.change_field_name("address", contact.address)
+        self.change_field_name("home", contact.home_phone)
+        self.change_field_name("email", contact.email)
+
+    def change_field_name(self, field_name, text):
         wd = self.app.wd
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home_phone)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def create(self, contact):
         wd = self.app.wd

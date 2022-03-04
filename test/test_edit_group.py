46f2d3fdd726384f8c610e82f2ef3ccfb08006b6
wswerlu <popovpn7@gmail.com>
2022-03-04 22:23:09 +0300

    refactor test_edit_group.py: add new tests

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,8 +1,19 @@
 from model.group import Group
 
 
-def test_edit_first_group(app):
-    app.open_home_page()
+def test_edit_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="test edit", header="test edit", footer="test edit"))
+    app.group.edit_first_group(Group(name="test edit"))
+    app.session.logout()
+
+
+def test_edit_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header="test edit"))
+    app.session.logout()
+
+
+def test_edit_group_footer(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(footer="test edit"))
     app.session.logout()

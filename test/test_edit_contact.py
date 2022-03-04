9c8734a385838a5c8002aedc82532a96a0a7b469
wswerlu <popovpn7@gmail.com>
2022-03-04 22:39:33 +0300

    refactor test_edit_contact.py: add new tests

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,8 +1,19 @@
 from model.contact import Contact
 
 
-def test_edit_first_contact(app):
+def test_edit_contact_firstname(app):
     app.session.login(username="admin", password="secret")
-    app.contact.edit_first_contact(Contact(firstname="test edit", lastname="test edit", address="test edit",
-                                           home_phone="test edit", email="test edit"))
+    app.contact.edit_first_contact(Contact(firstname="test edit"))
+    app.session.logout()
+
+
+def test_edit_contact_address(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first_contact(Contact(address="test edit"))
+    app.session.logout()
+
+
+def test_edit_contact_email(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first_contact(Contact(email="test edit"))
     app.session.logout()

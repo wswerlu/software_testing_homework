589b2b723d3227ae463180dd1139a210bc416f18
wswerlu <popovpn7@gmail.com>
2022-03-05 18:53:00 +0300

    refactor test_del_group.py

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,6 +2,5 @@ from model.group import Group
 
 
 def test_delete_first_group(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test delete"))
+    app.group.is_group_present(Group(name="test delete"))
     app.group.delete_first_group()

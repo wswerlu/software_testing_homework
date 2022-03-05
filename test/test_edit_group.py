8178b286b5a0a49521a269564a6d822403f2a330
wswerlu <popovpn7@gmail.com>
2022-03-05 18:56:55 +0300

    refactor test_edit_group.py

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,12 +2,15 @@ from model.group import Group
 
 
 def test_edit_group_name(app):
+    app.group.is_group_present(Group(name="test"))
     app.group.edit_first_group(Group(name="test edit"))
 
 
 def test_edit_group_header(app):
+    app.group.is_group_present(Group(name="test"))
     app.group.edit_first_group(Group(header="test edit"))
 
 
 def test_edit_group_footer(app):
+    app.group.is_group_present(Group(name="test"))
     app.group.edit_first_group(Group(footer="test edit"))

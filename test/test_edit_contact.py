9e8ad4d10e497b8a13e1c876a33168ee61f79281
wswerlu <popovpn7@gmail.com>
2022-03-05 19:10:39 +0300

    refactor test_edit_contact.py

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,12 +2,15 @@ from model.contact import Contact
 
 
 def test_edit_contact_firstname(app):
+    app.contact.is_contact_present(Contact(firstname="test"))
     app.contact.edit_first_contact(Contact(firstname="test edit"))
 
 
 def test_edit_contact_address(app):
+    app.contact.is_contact_present(Contact(firstname="test"))
     app.contact.edit_first_contact(Contact(address="test edit"))
 
 
 def test_edit_contact_email(app):
+    app.contact.is_contact_present(Contact(firstname="test"))
     app.contact.edit_first_contact(Contact(email="test edit"))

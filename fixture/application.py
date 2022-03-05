88a875bf4a706d3c623b372c048cfeebcb83646f
wswerlu <popovpn7@gmail.com>
2022-03-05 19:24:38 +0300

    refactor application.py

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,6 @@ from fixture.contact import ContactHelper
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(10)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)

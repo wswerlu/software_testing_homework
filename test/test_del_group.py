927ca5b0cc0e74f3fb739ca134df1b35de7fb62f
wswerlu <popovpn7@gmail.com>
2022-03-06 11:11:26 +0300

    refactor test_del_group.py and test_edit_group.py

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -2,5 +2,5 @@ from model.group import Group
 
 
 def test_delete_first_group(app):
-    app.group.is_group_present(Group(name="test delete"))
+    app.group.create_group_if_it_not_exist(Group(name="test delete"))
     app.group.delete_first_group()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,15 +2,15 @@ from model.group import Group
 
 
 def test_edit_group_name(app):
-    app.group.is_group_present(Group(name="test"))
+    app.group.create_group_if_it_not_exist(Group(name="test"))
     app.group.edit_first_group(Group(name="test edit"))
 
 
 def test_edit_group_header(app):
-    app.group.is_group_present(Group(name="test"))
+    app.group.create_group_if_it_not_exist(Group(name="test"))
     app.group.edit_first_group(Group(header="test edit"))
 
 
 def test_edit_group_footer(app):
-    app.group.is_group_present(Group(name="test"))
+    app.group.create_group_if_it_not_exist(Group(name="test"))
     app.group.edit_first_group(Group(footer="test edit"))

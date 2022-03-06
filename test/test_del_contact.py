4c6bb269734abbc471fef382f4b3de6d6c7345ce
wswerlu <popovpn7@gmail.com>
2022-03-06 11:12:51 +0300

    refactor test_del_contact.py and test_edit_contact.py

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -2,5 +2,5 @@ from model.contact import Contact
 
 
 def test_delete_first_contact(app):
-    app.contact.is_contact_present(Contact(firstname="test"))
+    app.contact.create_contact_if_is_not_exist(Contact(firstname="test"))
     app.contact.delete_first_contact()

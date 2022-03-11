0b62d143cebb7bb2a67175804d26fc5a16935257
Petr Popov <petyap666@gmail.com>
2022-03-11 22:17:58 +0300

    refactor test_add_group.py, test_delete_group.py, test_edit_group.py: add asserts

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,14 +3,23 @@ from model.group import Group
 
 def test_edit_group_name(app):
     app.group.create_group_if_it_not_exist(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="test edit"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_header(app):
     app.group.create_group_if_it_not_exist(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(header="test edit"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_group_footer(app):
     app.group.create_group_if_it_not_exist(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(footer="test edit"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)

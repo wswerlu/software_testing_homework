5d99bd59865f939ef87edd695f8c5f35d859873a
Petr Popov <petyap666@gmail.com>
2022-03-11 23:21:21 +0300

    refactor model/contact.py

--- model/contact.py
+++ model/contact.py
@@ -1,7 +1,8 @@
 class Contact:
-    def __init__(self, firstname=None, lastname=None, address=None, home_phone=None, email=None):
+    def __init__(self, firstname=None, lastname=None, address=None, home_phone=None, email=None, id=None):
         self.firstname = firstname
         self.lastname = lastname
         self.address = address
         self.home_phone = home_phone
         self.email = email
+        self.id = id

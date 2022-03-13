b825b5c86921cfa72e3ddcddeb3eafab272604cf
Petr Popov <petyap666@gmail.com>
2022-03-13 16:47:13 +0300

    refactor test_add_group.py, test_delete_group.py, test_edit_group.py

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,37 +1,41 @@
 from model.group import Group
+from random import randrange
 
 
 def test_edit_group_name(app):
     app.group.create_group_if_it_not_exist(Group(name="test"))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(name="test edit")
-    group.id = old_groups[0].id
-    app.group.edit_first_group(group)
+    group.id = old_groups[index].id
+    app.group.edit_group_by_index(group, index)
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_edit_group_header(app):
     app.group.create_group_if_it_not_exist(Group(name="test"))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(header="test edit")
-    group.id = old_groups[0].id
-    app.group.edit_first_group(group)
+    group.id = old_groups[index].id
+    app.group.edit_group_by_index(group, index)
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_edit_group_footer(app):
     app.group.create_group_if_it_not_exist(Group(name="test"))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(footer="test edit")
-    group.id = old_groups[0].id
-    app.group.edit_first_group(group)
+    group.id = old_groups[index].id
+    app.group.edit_group_by_index(group, index)
     assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)

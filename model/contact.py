9d7efbc06e86ffc26722fd55b33948f89732323b
Petr Popov <petyap666@gmail.com>
2022-03-13 15:13:43 +0300

    refactor model/contact.py: add methods __repr__, __eq__, id_or_max

--- model/contact.py
+++ model/contact.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+
+
 class Contact:
     def __init__(self, firstname=None, lastname=None, address=None, home_phone=None, email=None, id=None):
         self.firstname = firstname
@@ -6,3 +9,17 @@ class Contact:
         self.home_phone = home_phone
         self.email = email
         self.id = id
+
+    def __repr__(self):
+        return f"{self.id}:{self.firstname}:{self.lastname}"
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) \
+               and (self.firstname is None or other.firstname is None or self.firstname == other.firstname) \
+               and (self.lastname is None or other.lastname is None or self.lastname == other.lastname)
+
+    def id_or_max(self):
+        if self.id is not None:
+            return int(self.id)
+        else:
+            return maxsize

988dbaef2ad10ec8969fb106b7e48d562484002f
Petr Popov <petyap666@gmail.com>
2022-03-13 15:56:19 +0300

    refactor test_add_group.py, test_delete_group.py, test_edit_group.py: add hashing

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -7,8 +7,8 @@ def test_edit_group_name(app):
     group = Group(name="test edit")
     group.id = old_groups[0].id
     app.group.edit_first_group(group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
     old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
@@ -19,8 +19,8 @@ def test_edit_group_header(app):
     group = Group(header="test edit")
     group.id = old_groups[0].id
     app.group.edit_first_group(group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
     old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
@@ -31,7 +31,7 @@ def test_edit_group_footer(app):
     group = Group(footer="test edit")
     group.id = old_groups[0].id
     app.group.edit_first_group(group)
+    assert len(old_groups) == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
     old_groups[0] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)

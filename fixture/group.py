d4a093352d88f8ab4760a295d9b50e22af16b874
Petr Popov <petyap666@gmail.com>
2022-03-13 16:45:18 +0300

    refactor fixture/group.py: add methods select_group_by_index, edit_group_by_index, delete_group_by_index

--- fixture/group.py
+++ fixture/group.py
@@ -39,23 +39,32 @@ class GroupHelper:
             wd.find_element_by_name(field_name).send_keys(text)
 
     def delete_first_group(self):
+        self.delete_group_by_index(0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
         self.group_cache = None
 
     def select_first_group(self):
+        self.select_group_by_index(0)
+
+    def select_group_by_index(self, index):
         wd = self.app.wd
-        wd.find_element_by_name("selected[]").click()
+        wd.find_elements_by_name("selected[]")[index].click()
 
     def edit_first_group(self, new_group_data):
+        self.edit_group_by_index(0)
+
+    def edit_group_by_index(self, new_group_data, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
-        # init first group editing
+        self.select_group_by_index(index)
+        # init group editing
         wd.find_element_by_name("edit").click()
         self.fill_group_form(new_group_data)
         # submit group editing

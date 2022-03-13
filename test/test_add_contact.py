35699335b79a10d7718a952a44ae8c6f2d050761
Petr Popov <petyap666@gmail.com>
2022-03-13 16:05:34 +0300

    refactor test_add_contact.py, test_delete_contact.py, test_edit_contact.py: add hashing

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -7,7 +7,7 @@ def test_add_contact(app):
     contact = Contact(firstname="test create", lastname="test create", address="test create",
                                home_phone="test create", email="test create")
     app.contact.create(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -7,8 +7,8 @@ def test_edit_contact_firstname(app):
     contact = Contact(firstname="test edit")
     contact.id = old_contacts[0].id
     app.contact.edit_first_contact(contact)
+    assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
@@ -19,8 +19,8 @@ def test_edit_contact_address(app):
     contact = Contact(address="test edit")
     contact.id = old_contacts[0].id
     app.contact.edit_first_contact(contact)
+    assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
@@ -31,7 +31,7 @@ def test_edit_contact_email(app):
     contact = Contact(email="test edit")
     contact.id = old_contacts[0].id
     app.contact.edit_first_contact(contact)
+    assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
     old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)

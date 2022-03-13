1f13f945824c7c2bc47529d0a5194d92ecb8a29b
Petr Popov <petyap666@gmail.com>
2022-03-13 15:13:04 +0300

    refactor test_add_contact.py, test_del_group.py, test_edit_group.py: add asserts

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,22 +4,34 @@ from model.group import Group
 def test_edit_group_name(app):
     app.group.create_group_if_it_not_exist(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name="test edit"))
+    group = Group(name="test edit")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_edit_group_header(app):
     app.group.create_group_if_it_not_exist(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(header="test edit"))
+    group = Group(header="test edit")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_edit_group_footer(app):
     app.group.create_group_if_it_not_exist(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(footer="test edit"))
+    group = Group(footer="test edit")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)

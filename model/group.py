d2b23f12b27b461a6d8cc0203900acae0a3d7310
Petr Popov <petyap666@gmail.com>
2022-03-13 15:10:23 +0300

    refactor model/group.py: add methods __repr__, __eq__, id_or_max

--- model/group.py
+++ model/group.py
@@ -1,6 +1,22 @@
+from sys import maxsize
+
+
 class Group:
     def __init__(self, name=None, header=None, footer=None, id=None):
         self.name = name
         self.header = header
         self.footer = footer
         self.id = id
+
+    def __repr__(self):
+        return f"{self.id}:{self.name}"
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) and \
+               (self.name is None or other.name is None or self.name == other.name)
+
+    def id_or_max(self):
+        if self.id is not None:
+            return int(self.id)
+        else:
+            return maxsize

25931574a1dc6e9cb41b0fde517133d68fb57e86
Petr Popov <petyap666@gmail.com>
2022-03-19 22:22:38 +0300

    refactor model/group.py

--- model/group.py
+++ model/group.py
@@ -9,7 +9,7 @@ class Group:
         self.id = id
 
     def __repr__(self):
-        return f"{self.id}:{self.name}"
+        return f"{self.id}:{self.name}:{self.header}:{self.footer}"
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and \

641960082585a89013ca251c24a62c8c19465834
Petr Popov <petyap666@gmail.com>
2022-03-19 20:48:21 +0300

    refactor test_phones.py

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,10 +1,8 @@
-import re
-
-
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_phones_from_home_page == \
+           app.contact.merge_phones_like_on_home_page(contact_from_edit_page)
 
 
 def test_phones_on_contact_view_page(app):
@@ -14,15 +12,3 @@ def test_phones_on_contact_view_page(app):
     assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
     assert contact_from_view_page.workphone == contact_from_edit_page.workphone
     assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
-
-
-def clear(string):
-    return re.sub("[() -]", "", string)
-
-
-def merge_phones_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None,
-                                       [contact.homephone, contact.mobilephone,
-                                        contact.workphone, contact.secondaryphone]))))

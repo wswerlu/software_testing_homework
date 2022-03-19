4de86172fd8bb69f01eab0b1d1f9e960987d1e65
Petr Popov <petyap666@gmail.com>
2022-03-19 22:24:41 +0300

    refactor test_add_contact.py: add parametrize

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", lastname="", address="",
+                      homephone="", email="")] + [
+    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 15),
+            address=random_string("address", 20), homephone=random_string("homephone", 15),
+            email=random_string("email", 15)) for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="test create", lastname="test create", address="test create",
-                      homephone="test create", email="test create")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()

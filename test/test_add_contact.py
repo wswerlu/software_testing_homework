91452e7af6fc62720832c024c878b8075c16bc66
Petr Popov <petyap666@gmail.com>
2022-03-25 23:54:58 +0300

    refactor test_add_contact.py

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,25 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", lastname="", address="",
-                      homephone="", email="")] + [
-    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 15),
-            address=random_string("address", 20), homephone=random_string("homephone", 15),
-            email=random_string("email", 15)) for i in range(5)
-]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()

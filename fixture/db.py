b59777dfe84b86aded133efd71ea9e438dfe0057
Petr Popov <petyap666@gmail.com>
2022-04-04 21:13:52 +0300

    refactor db.py: update sql query in method get_contact_list

--- fixture/db.py
+++ fixture/db.py
@@ -27,7 +27,8 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname, address, home, email from addressbook")
+            cursor.execute("select id, firstname, lastname, address, home, email from addressbook "
+                           "where deprecated = '0000-00-00 00:00:00'")
             for row in cursor:
                 (id, firstname, lastname, address, homephone, email) = row
                 list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address,

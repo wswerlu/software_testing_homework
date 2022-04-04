af96a6184ff867ef2be07df2eeaa6d08efed811b
Petr Popov <petyap666@gmail.com>
2022-04-04 20:51:24 +0300

    refactor test_add_contact.py: get data from database

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,14 +1,13 @@
 from model.contact import Contact
-from random import randrange
+import random
 
 
-def test_delete_some_contact(app):
+def test_delete_some_contact(app, db):
     app.contact.create_contact_if_it_not_exist(Contact(firstname="test"))
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    app.contact.delete_contact_by_index(index)
-    assert len(old_contacts) - 1 == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts[index:index+1] = []
+    old_contacts = db.get_contact_list()
+    contact = random.choice(old_contacts)
+    app.contact.delete_contact_by_id(contact.id)
+    new_contacts = db.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts.remove(contact)
     assert old_contacts == new_contacts
-

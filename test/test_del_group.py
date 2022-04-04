bdbff41209eab8145bc0ded6daf9a1197634726e
Petr Popov <petyap666@gmail.com>
2022-04-04 20:51:11 +0300

    refactor test_add_contact.py: get data from database and return assert ui

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,13 +1,15 @@
+import random
 from model.group import Group
-from random import randrange
 
 
-def test_delete_some_group(app):
+def test_delete_some_group(app, db, check_ui):
     app.group.create_group_if_it_not_exist(Group(name="test delete"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    app.group.delete_group_by_index(index)
-    assert len(old_groups) - 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index:index+1] = []
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    app.group.delete_group_by_id(group.id)
+    new_groups = db.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
+    old_groups.remove(group)
     assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)

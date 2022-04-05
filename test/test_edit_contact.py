d7433a907c42f431de6824645d01850a29641ee1
Petr Popov <petyap666@gmail.com>
2022-04-05 23:34:48 +0300

    refactor test_edit_contact.py: get data from database and return assert ui

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,41 +1,19 @@
 from model.contact import Contact
-from random import randrange
+import random
 
 
-def test_edit_contact_firstname(app):
+def test_edit_some_contact(app, db, data_contacts, check_ui):
+    contact = data_contacts
     app.contact.create_contact_if_it_not_exist(Contact(firstname="test"))
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    contact = Contact(firstname="test edit")
-    contact.id = old_contacts[index].id
-    app.contact.edit_contact_by_index(contact, index)
+    old_contacts = db.get_contact_list()
+    some_contact = random.choice(old_contacts)
+    index = old_contacts.index(some_contact)
+    contact.id = some_contact.id
+    app.contact.edit_contact_by_id(contact, some_contact.id)
     assert len(old_contacts) == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
     old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-def test_edit_contact_address(app):
-    app.contact.create_contact_if_it_not_exist(Contact(firstname="test"))
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    contact = Contact(address="test edit")
-    contact.id = old_contacts[index].id
-    app.contact.edit_contact_by_index(contact, index)
-    assert len(old_contacts) == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts[index] = contact
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
-
-def test_edit_contact_email(app):
-    app.contact.create_contact_if_it_not_exist(Contact(firstname="test"))
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    contact = Contact(email="test edit")
-    contact.id = old_contacts[index].id
-    app.contact.edit_contact_by_index(contact, index)
-    assert len(old_contacts) == app.contact.count()
-    new_contacts = app.contact.get_contact_list()
-    old_contacts[index] = contact
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) \
+               == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)

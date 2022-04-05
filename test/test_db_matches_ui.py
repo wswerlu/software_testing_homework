6872bf77b1ccb180da61c634cd22efb56b553f20
Petr Popov <petyap666@gmail.com>
2022-04-06 00:24:44 +0300

    refactor test_db_matches_ui.py: add test_contact_list

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,4 +1,5 @@
 from model.group import Group
+from model.contact import Contact
 
 
 def test_group_list(app, db):
@@ -7,3 +8,11 @@ def test_group_list(app, db):
         return Group(id=group.id, name=group.name.strip())
     db_list = map(clean, db.get_group_list())
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+
+def test_contact_list(app, db):
+    ui_list = app.contact.get_contact_list()
+    def clean(contact):
+        return Contact(id=contact.id, firstname=contact.firstname.strip(), lastname=contact.lastname.strip())
+    db_list = map(clean, db.get_contact_list())
+    assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)

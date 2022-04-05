ba3ec143ce0d0f0c1a012755e04fa8ed34e3dcae
Petr Popov <petyap666@gmail.com>
2022-04-05 22:52:42 +0300

    update groups.py and contacts.py

--- data/contacts.py
+++ data/contacts.py
@@ -2,8 +2,7 @@ from model.contact import Contact
 
 
 testdata = [
-    Contact(firstname="firstname1", lastname="lastname1", address="address1", homephone="homephone1",
-            email="email1"),
-    Contact(firstname="firstname2", lastname="lastname2", address="address2", homephone="homephone2",
-            email="email2")
+    Contact(firstname="firstname"),
+    Contact(address="address"),
+    Contact(email="email")
 ]
--- data/groups.py
+++ data/groups.py
@@ -2,6 +2,7 @@ from model.group import Group
 
 
 testdata = [
-    Group(name="name1", header="header1", footer="footer1"),
-    Group(name="name2", header="header2", footer="footer2")
+    Group(name="name"),
+    Group(header="header"),
+    Group(footer="footer")
 ]

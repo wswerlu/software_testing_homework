64b1a127d35a37dbe853ed7e32fbe6c4bf3df6b1
Petr Popov <petyap666@gmail.com>
2022-04-05 23:34:24 +0300

    refactor fixture/contact.py: add methods edit_contact_by_id and open_contact_to_edit_by_id

--- fixture/contact.py
+++ fixture/contact.py
@@ -76,11 +76,27 @@ class ContactHelper:
         self.return_to_home_page()
         self.contact_cache = None
 
+    def edit_contact_by_id(self, contact, id):
+        wd = self.app.wd
+        self.open_contact_page()
+        self.open_contact_to_edit_by_id(id)
+        self.fill_contact_form(contact)
+        # submit contact editing
+        wd.find_element_by_name("update").click()
+        self.return_to_home_page()
+        self.contact_cache = None
+
     def open_contact_to_edit_by_index(self, index):
         wd = self.app.wd
         self.open_contact_page()
         # init contact editing
         wd.find_elements_by_css_selector("[alt='Edit']")[index].click()
+
+    def open_contact_to_edit_by_id(self, id):
+        wd = self.app.wd
+        self.open_contact_page()
+        # init contact editing
+        wd.find_element_by_xpath(f"//a[contains(@href, '{id}')]/img[@alt='Edit']").click()
 
     def open_contact_details_by_index(self, index):
         wd = self.app.wd

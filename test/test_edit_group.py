e266801f38e473a09a737616df01415676c929d6
Petr Popov <petyap666@gmail.com>
2022-04-05 23:33:41 +0300

    refactor test_edit_group.py: get data from database and return assert ui

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,41 +1,18 @@
 from model.group import Group
-from random import randrange
+import random
 
 
-def test_edit_group_name(app):
+def test_edit_some_group(app, db, data_groups, check_ui):
+    group = data_groups
     app.group.create_group_if_it_not_exist(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(name="test edit")
-    group.id = old_groups[index].id
-    app.group.edit_group_by_index(group, index)
+    old_groups = db.get_group_list()
+    some_group = random.choice(old_groups)
+    index = old_groups.index(some_group)
+    group.id = some_group.id
+    app.group.edit_group_by_id(group, some_group.id)
     assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-def test_edit_group_header(app):
-    app.group.create_group_if_it_not_exist(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(header="test edit")
-    group.id = old_groups[index].id
-    app.group.edit_group_by_index(group, index)
-    assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-def test_edit_group_footer(app):
-    app.group.create_group_if_it_not_exist(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(footer="test edit")
-    group.id = old_groups[index].id
-    app.group.edit_group_by_index(group, index)
-    assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)

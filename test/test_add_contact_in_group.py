80787a457d40ac562628fd96ac266cafa308ca72
Petr Popov <petyap666@gmail.com>
2022-04-10 00:05:03 +0300

    refactor test_add_contact_in_group.py: add contact that was not in group initially

--- test/test_add_contact_in_group.py
+++ test/test_add_contact_in_group.py
@@ -6,9 +6,9 @@ import random
 def test_add_some_contact_in_some_group(app, db, orm):
     app.group.create_group_if_it_not_exist(Group(name="test"))
     app.contact.create_contact_if_it_not_exist(Contact(firstname="test"))
-    contacts = db.get_contact_list()
     groups = db.get_group_list()
-    contact = random.choice(contacts)
     group = random.choice(groups)
+    contacts = orm.get_contacts_not_in_group(group)
+    contact = random.choice(contacts)
     app.contact.add_contact_in_group(contact.id, group.id)
     assert contact in orm.get_contacts_in_group(group)

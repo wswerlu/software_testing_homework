e3e66cd7ebdcf1f02c26d89553cfea666fa1aa75
Petr Popov <petyap666@gmail.com>
2022-04-09 16:24:46 +0300

    refactor test_contact_info_on_home_page.py: add test test_contact_info_on_home_page_with_ui_and_db

--- test/test_contact_info_on_home_page.py
+++ test/test_contact_info_on_home_page.py
@@ -1,7 +1,8 @@
 from random import randrange
+from model.contact import Contact
 
 
-def test_contact_info_on_home_page(app):
+def test_contact_info_on_home_page_with_ui(app):
     contacts = app.contact.get_contact_list()
     index = randrange(len(contacts))
     contact_from_home_page = app.contact.get_contact_list()[index]
@@ -13,3 +14,18 @@ def test_contact_info_on_home_page(app):
            app.contact.merge_phones_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_emails_from_home_page == \
            app.contact.merge_emails_like_on_home_page(contact_from_edit_page)
+
+
+def test_contact_info_on_home_page_with_ui_and_db(app, db):
+    contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    contacts_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    for index in range(len(contacts_from_home_page)):
+        contact_from_home_page = contacts_from_home_page[index]
+        contact_from_db = contacts_from_db[index]
+        assert contact_from_home_page.firstname == contact_from_db.firstname.strip()
+        assert contact_from_home_page.lastname == contact_from_db.lastname.strip()
+        assert contact_from_home_page.address == contact_from_db.address.strip()
+        assert contact_from_home_page.all_phones_from_home_page == \
+               app.contact.merge_phones_like_on_home_page(contact_from_db)
+        assert contact_from_home_page.all_emails_from_home_page == \
+               app.contact.merge_emails_like_on_home_page(contact_from_db)

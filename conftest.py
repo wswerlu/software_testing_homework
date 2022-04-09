1f77c1dbb322774a8b4fe3c6770b4b4f54c4cecc
Petr Popov <petyap666@gmail.com>
2022-04-09 21:07:37 +0300

    refactor conftest.py: add orm fixture

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ import os.path
 import importlib
 from fixture.application import Application
 from fixture.db import DbFixture
+from fixture.orm import ORMFixture
 
 
 fixture = None
@@ -40,6 +41,14 @@ def db(request):
         dbfixture.destroy()
     request.addfinalizer(fin)
     return dbfixture
+
+
+@pytest.fixture(scope="session")
+def orm(request):
+    db_config = load_config(request.config.getoption("--target"))["db"]
+    ormfixture = ORMFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"],
+                          password=db_config["password"])
+    return ormfixture
 
 
 @pytest.fixture(scope="session", autouse=True)
